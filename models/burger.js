08bd31279ee60b657ed9d809a7f69862c99dfcc0
kyle074613 <kylemj@uci.edu>
2020-02-23 15:43:16 -0800

    finished burger model, started burger controller, neither have been tested

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require("../config/orm.js");
+
+const cat = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    insertOne: function (col, val, cb) {
+        orm.insertOne("burgers", col, val, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (col, val, condition, cb) {
+        orm.updateOne("burgers", col, val, condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = cat;
